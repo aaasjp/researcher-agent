9e4e04b7cb7c0406dd6abfddf61ddb59a4084853
songjianping <aaasjp@126.com>
2025-03-05 10:03:35 +0800

    bug fix

--- tests/researcher_agent_test.py
+++ tests/researcher_agent_test.py
@@ -3,9 +3,9 @@ from react_agent.graph import graph
 from langchain_core.messages import HumanMessage, SystemMessage
 
 async def main():
-
+    config = {"configurable": {"thread_id": "1", "max_search_results": 1}}
     messages = [HumanMessage(content="介绍一下deepseek")]
-    result = await graph.ainvoke({"messages": messages})
+    result = await graph.ainvoke({"messages": messages},config)
 
 if __name__ == "__main__":
     asyncio.run(main())
